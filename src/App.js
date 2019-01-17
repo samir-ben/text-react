ab7e7874683060f1886bb39814e8bf8a27efcfa5
samir-ben <samir-69@hotmail.fr>
2019-01-17 15:52:23 +0100

    feat: navbar
    - add navbar template
    - add navbar in app root
    - config router in app root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-     salut
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Switch>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
