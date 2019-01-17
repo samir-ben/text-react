232257c8c3a92ccc633eb0daed59b9b38499e640
samir-ben <samir-69@hotmail.fr>
2019-01-17 16:09:41 +0100

    feat: navbar links
    - add dynamics links in navbar component

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
+
+const SignedInLinks = props => {
+  return (
+    <div>
+      <ul className='right'>
+        <li>
+          <NavLink to='/add'>Nouveau commentaire</NavLink>
+        </li>
+        <li>
+          <a href='/' onClick={props.signOut}>
+            Déconnexion
+          </a>
+        </li>
+        <li>
+          <NavLink to='/' className='btn btn-floating pink lighten-1'>
+            {props.profile.initials}
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignedInLinks);
