62c21c56696991a6f74716e1b58a63339eb9a680
samir-ben <samir-69@hotmail.fr>
2019-01-22 01:07:14 +0100

    feat: sidenav-trigger
    - add sidenav-trigger for mobile device

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 const Navbar = (props) => {
@@ -12,18 +12,23 @@ const Navbar = (props) => {
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo"><span style={{ color: '#039BE5' }}>M</span>es<span style={{ color: '#039BE5' }}>A</span>nnotations</Link>
-                {links}
+                <Link to="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+                <ul className="hide-on-med-and-down">
+                    {links}
+                </ul>  
             </div>
+            <ul className="sidenav" id="mobile-demo">
+                {links}
+            </ul>
         </nav>
     )
 }
 
 const mapStateToProps = (state) => {
-    // console.log(state);
     return {
         auth: state.firebase.auth,
         profile: state.firebase.profile
     }
 }
 
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(withRouter(Navbar))
